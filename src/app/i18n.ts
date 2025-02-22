1839d7af481538d6b5db998e8e3b071a5ec81e79
dosterevan <dosterevan@gmail.com>
2025-02-21 23:18:01 -0800

    Lowering Dependecy Usage.

--- src/app/i18n.ts
+++ src/app/i18n.ts
@@ -1,28 +1,28 @@
-"use client";
+// "use client";
 
-import i18n from 'i18next';
+// import i18n from 'i18next';
 
-import { initReactI18next } from 'react-i18next';
-import LanguageDetector from 'i18next-browser-languagedetector';
-import HTTPApi from "i18next-http-backend";
-import enTranslations from '../locales/en.json';
-import fiTranslations from '../locales/fi.json';
+// import { initReactI18next } from 'react-i18next';
+// import LanguageDetector from 'i18next-browser-languagedetector';
+// import HTTPApi from "i18next-http-backend";
+// import enTranslations from '../locales/en.json';
+// import fiTranslations from '../locales/fi.json';
 
-i18n
-  .use(LanguageDetector)
-  .use(initReactI18next)
-  .use(HTTPApi)
-  .init({
-    resources: {
-      en: { translation: enTranslations },
-      fi: { translation: fiTranslations },
-    },
-    fallbackLng: 'en',
-    interpolation: { escapeValue: false },
-    detection: {
-      order: ['querystring', 'cookie', 'localStorage', 'navigator'],
-      caches: ['localStorage', 'cookie'],
-    },
-  });
+// i18n
+//   .use(LanguageDetector)
+//   .use(initReactI18next)
+//   .use(HTTPApi)
+//   .init({
+//     resources: {
+//       en: { translation: enTranslations },
+//       fi: { translation: fiTranslations },
+//     },
+//     fallbackLng: 'en',
+//     interpolation: { escapeValue: false },
+//     detection: {
+//       order: ['querystring', 'cookie', 'localStorage', 'navigator'],
+//       caches: ['localStorage', 'cookie'],
+//     },
+//   });
 
-export default i18n;
+// export default i18n;
